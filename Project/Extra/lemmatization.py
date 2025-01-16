f635a6f2b1f2165c77de5903184c9dc913b341f0
Frosso Varsou <frossovarsou@gmail.com>
2025-01-16 16:43:33 +0200

    Add commented-out code for collecting titles and descriptions

--- Project/Extra/lemmatization.py
+++ Project/Extra/lemmatization.py
@@ -15,3 +15,10 @@ for d in collected_data:
         "lemmed_tokens": lemmed_tokens
     })
 lemmed_data[1]
+
+  # Συλλογή τίτλων και περιγραφών
+        # for item in soup.select('.mw-parser-output > p'):
+        #     title = soup.title.string
+        #     description = item.get_text(strip=True)
+        #     if description:
+        #         data.append({'title': title, 'description': description})
